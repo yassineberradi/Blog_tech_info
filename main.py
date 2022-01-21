0f255d2371e1e26e71d0d2e0df9233d92aaa32e7
yassine <berradi.yassine@gmail.com>
2022-01-22 00:26:33 +0100

    add postgrad

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ print(MY_EMAIL)
 print(MY_PASSWORD)
 print(TO_EMAIL)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
-##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
